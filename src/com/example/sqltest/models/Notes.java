9914d354adb0aaf9f4832a08925068e679532db6
Breena Theseira <breenatheseira@yahoo.com>
2015-05-02 15:29:07 +0800

    Added a new package for models and a new Notes class

---
+++ src/com/example/sqltest/models/Notes.java
@@ -0,0 +1,37 @@
+package com.example.sqltest.models;
+
+public class Notes {
+	String id, doc_id, note;
+	
+	public Notes(){}
+	
+	public Notes(String id, String doc_id, String note){
+		this.id = id;
+		this.doc_id = doc_id;
+		this.note = note;
+	}
+	
+	public void setId(String id){
+		this.id = id;
+	}
+	
+	public void setDocId(String did){
+		this.doc_id = did;
+	}
+	
+	public void setNote(String content){
+		this.note = content;
+	}
+	
+	public String getId(){
+		return this.id;
+	}
+	
+	public String getDocId(){
+		return this.doc_id;
+	}
+	
+	public String getNote(){
+		return this.note;
+	}
+}
