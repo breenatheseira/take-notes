dc6a2f5944543a72cc773baa29fe682c5f7a6666
Breena Theseira <breenatheseira@yahoo.com>
2015-05-02 15:22:26 +0800

    Updated: Icons are showing up at the action bar menu

--- src/com/example/sqltest/MainActivity.java
+++ src/com/example/sqltest/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends ActionBarActivity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.savenote, menu);
 		return true;
 	}
 
@@ -59,7 +59,7 @@ public class MainActivity extends ActionBarActivity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		if (id == R.id.save) {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
