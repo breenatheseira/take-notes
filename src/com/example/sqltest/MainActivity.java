601c42d1384732267a8aed668425812d890f1e04
Breena Theseira <breenatheseira@yahoo.com>
2015-05-02 19:30:33 +0800

    Updated: Add Note's view icon to include save note only

--- src/com/example/sqltest/MainActivity.java
+++ src/com/example/sqltest/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends ActionBarActivity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.viewnote, menu);
+		getMenuInflater().inflate(R.menu.addnote, menu);
 		return true;
 	}
 
